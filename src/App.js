031db9e18150a8e2688bc4cc8b4238c096ccb2cc
snehasamuel <snehasamuel297@gmail.com>
2022-05-23 12:19:51 +0530

    Header  branch completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Entry from './components/Entry';
+import Search from './components/Search';
+import View from './components/View';
 
 function App() {
   return (
     <>
-    welcome
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" exact element={<Entry/>}/>
+    <Route path="/search" exact element={<Search/>}/>
+    <Route path="/view" exact element={<View/>}/>
+  </Routes>
+  </BrowserRouter>
     </>
     
 
